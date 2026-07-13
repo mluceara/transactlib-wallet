5c23dbf69ef9020295e08fe306c076fabc5ea58b
mlucenara <lucenara@gmail.com>
2026-07-13 20:53:04 +0800

    chore: initialize Spring Boot project with Gradle build

---
+++ src/main/java/com/transactlib/wallet/TransactLib/TransactLibApplication.java
@@ -0,0 +1,13 @@
+package com.transactlib.wallet.TransactLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactLibApplication.class, args);
+	}
+
+}
